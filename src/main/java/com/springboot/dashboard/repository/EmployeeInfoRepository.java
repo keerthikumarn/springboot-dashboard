35b23d78937895cce5b99807aed818935b3b88b5
Keerthi Kumar N <keerthi.narayan@subex.com>
2024-04-12 13:06:26 +0530

    created the employeeinfo repo

---
+++ src/main/java/com/springboot/dashboard/repository/EmployeeInfoRepository.java
@@ -0,0 +1,11 @@
+package com.springboot.dashboard.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springboot.dashboard.entity.EmployeeInfo;
+
+@Repository
+public interface EmployeeInfoRepository extends JpaRepository<EmployeeInfo, Long> {
+
+}
