e9499557d9dd57a69a4fdaec99a5b8328d7ab6fa
Keerthi Kumar N <keerthi.narayan@subex.com>
2024-04-12 18:11:24 +0530

    implemented the basic findAll

--- src/main/java/com/springboot/dashboard/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/springboot/dashboard/service/impl/DashboardServiceImpl.java
@@ -3,28 +3,42 @@ package com.springboot.dashboard.service.impl;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.springboot.dashboard.entity.CompanyRevenue;
 import com.springboot.dashboard.entity.EmployeeInfo;
 import com.springboot.dashboard.entity.OrderCollectionStatus;
 import com.springboot.dashboard.entity.OrderReceived;
 import com.springboot.dashboard.entity.ProductCategory;
+import com.springboot.dashboard.repository.CompanyRevenueRepository;
+import com.springboot.dashboard.repository.OrderReceivedRepository;
+import com.springboot.dashboard.repository.ProductCategoryRepository;
 import com.springboot.dashboard.service.DashboardService;
 
 public class DashboardServiceImpl implements DashboardService {
 
+	@Autowired
+	private CompanyRevenueRepository companyReveneueRepo;
+
+	@Autowired
+	private ProductCategoryRepository productCategoryRepo;
+
+	@Autowired
+	private OrderReceivedRepository orderRepo;
+
 	@Override
 	public List<CompanyRevenue> getTodayRevenueDashSimple() {
-		return null;
+		return companyReveneueRepo.findAll();
 	}
 
 	@Override
 	public List<ProductCategory> getBestCategorySimple() {
-		return null;
+		return productCategoryRepo.findAll();
 	}
 
 	@Override
 	public List<OrderReceived> getAllOrderReceivedSimple() {
-		return null;
+		return orderRepo.findAll();
 	}
 
 	@Override
