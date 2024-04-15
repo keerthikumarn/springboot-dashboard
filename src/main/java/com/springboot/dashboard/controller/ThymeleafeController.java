5dde12edd7fe969f01fb33772d96df4fa7f8a3ff
keerthikumarn <n.keerthikumar85@gmail.com>
2024-04-15 16:47:36 +0530

    implemented the thymeleaf controller

--- src/main/java/com/springboot/dashboard/controller/ThymeleafeController.java
+++ src/main/java/com/springboot/dashboard/controller/ThymeleafeController.java
@@ -1,6 +1,11 @@
 package com.springboot.dashboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.dashboard.service.DashboardService;
 
 /**
  * 
@@ -8,4 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ThymeleafeController {
 
+	@Autowired
+	private DashboardService dashboardService;
+
+	@GetMapping("/dashboard")
+	public String getDashboardDetails(Model model) {
+		model.addAttribute("cr", dashboardService.getTodayRevenueDash());
+		model.addAttribute("ei", dashboardService.getAllEmployee());
+		model.addAttribute("bc", dashboardService.getBestCategory());
+		model.addAttribute("or", dashboardService.getAllOrderReceived());
+		model.addAttribute("os", dashboardService.getOrderCollection());
+		return "/dashboard";
+	}
 }
