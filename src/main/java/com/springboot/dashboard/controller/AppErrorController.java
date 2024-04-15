078ad4945039d610fcea99d63a4014b503c784a7
keerthikumarn <n.keerthikumar85@gmail.com>
2024-04-15 15:00:06 +0530

    implemented getTraceParameter and getErrorAttributes API

--- src/main/java/com/springboot/dashboard/controller/AppErrorController.java
+++ src/main/java/com/springboot/dashboard/controller/AppErrorController.java
@@ -1,10 +1,16 @@
 package com.springboot.dashboard.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,17 +23,29 @@ public class AppErrorController implements ErrorController {
 	@Autowired
 	private ErrorAttributes errorAttributes;
 
-	@RequestMapping(value = ERROR_PATH, produces = "text/html")
+	@GetMapping(value = ERROR_PATH, produces = "text/html")
 	public ModelAndView errorHtml(HttpServletRequest request) {
 		return new ModelAndView("/errors/error", getErrorAttributes(request, false));
 	}
 
-	@RequestMapping(value = ERROR_PATH)
-	@ResponseBody
+	@GetMapping(value = ERROR_PATH)
 	public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
 		Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
 		HttpStatus status = getStatus(request);
 		return new ResponseEntity<Map<String, Object>>(body, status);
+	}
+
+	private boolean getTraceParameter(HttpServletRequest request) {
+		String parameter = request.getParameter("trace");
+		if (parameter == null) {
+			return false;
+		}
+		return !"false".equals(parameter.toLowerCase());
+	}
+
+	private Map<String, Object> getErrorAttributes(HttpServletRequest request, boolean includeStackTrace) {
+		WebRequest webRequest = new ServletWebRequest(request);
+		return this.errorAttributes.getErrorAttributes(webRequest, includeStackTrace);
 	}
 
 }
