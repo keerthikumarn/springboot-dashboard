ed0c7bffa7a3dec5ff1204199ee952fea8d9ad47
keerthikumarn <n.keerthikumar85@gmail.com>
2024-04-15 14:51:07 +0530

    implemented login denied

--- src/main/java/com/springboot/dashboard/controller/LoginController.java
+++ src/main/java/com/springboot/dashboard/controller/LoginController.java
@@ -5,17 +5,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-	
+
 	@GetMapping("/")
 	public String landingPage() {
 		return "login";
 	}
-	
+
 	@GetMapping("/login")
 	public String loginPage() {
 		return "login";
 	}
-	
-	
+
+	@GetMapping("/access-denied")
+	public String accessDenied() {
+		return "/access-denied";
+	}
 
 }
