31cd12d391bb419b81de2ea9ab9438bcddb11276
keerthikumarn <n.keerthikumar85@gmail.com>
2024-04-15 14:55:41 +0530

    added new apis

--- src/main/java/com/springboot/dashboard/controller/AppErrorController.java
+++ src/main/java/com/springboot/dashboard/controller/AppErrorController.java
@@ -3,16 +3,31 @@ package com.springboot.dashboard.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 public class AppErrorController implements ErrorController {
 
 	private final static String ERROR_PATH = "/error";
-	
+
 	@Autowired
 	private ErrorAttributes errorAttributes;
-	
-	
+
+	@RequestMapping(value = ERROR_PATH, produces = "text/html")
+	public ModelAndView errorHtml(HttpServletRequest request) {
+		return new ModelAndView("/errors/error", getErrorAttributes(request, false));
+	}
+
+	@RequestMapping(value = ERROR_PATH)
+	@ResponseBody
+	public ResponseEntity<Map<String, Object>> error(HttpServletRequest request) {
+		Map<String, Object> body = getErrorAttributes(request, getTraceParameter(request));
+		HttpStatus status = getStatus(request);
+		return new ResponseEntity<Map<String, Object>>(body, status);
+	}
 
 }
