eb83a7e8e2b82732458cc58cd134e35782dc722f
keerthikumarn <n.keerthikumar85@gmail.com>
2024-04-15 14:54:12 +0530

    started with the AppErrorController

--- src/main/java/com/springboot/dashboard/controller/AppErrorController.java
+++ src/main/java/com/springboot/dashboard/controller/AppErrorController.java
@@ -1,8 +1,18 @@
 package com.springboot.dashboard.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class AppErrorController {
+public class AppErrorController implements ErrorController {
+
+	private final static String ERROR_PATH = "/error";
+	
+	@Autowired
+	private ErrorAttributes errorAttributes;
+	
+	
 
 }
