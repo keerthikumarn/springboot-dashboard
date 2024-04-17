dcc4295e913d1416e4c5f753d2b630b1b212338e
keerthikumarn <n.keerthikumar85@gmail.com>
2024-04-17 20:37:18 +0530

    updated files

--- src/main/java/com/springboot/dashboard/SpringbootDashboardApplication.java
+++ src/main/java/com/springboot/dashboard/SpringbootDashboardApplication.java
@@ -2,8 +2,12 @@ package com.springboot.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.springboot.dashboard.entity")
+@EnableJpaRepositories(basePackages = "com.springboot.dashboard.repository")
 public class SpringbootDashboardApplication {
 
 	public static void main(String[] args) {
